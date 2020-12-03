611a118ae3091c25c73938389c3001eae941e684
Gaston Corvalan <corvata@gmail.com>
2020-12-03 20:42:50 -0300

    par impar

--- clase3.js
+++ clase3.js
@@ -4,7 +4,7 @@ for (var i=0; i<7; i++){
     if(i==6){
         alert("Hoy es  " + dias[i] + "  septimo dia de la semana");
         console.log(dias[i] + " semana terminada");
-    }else if(i % 2){
+    }else if(i % 2 == 0){
         alert(dias[i] + " es un dia par");
         console.log(dias[i] + "  es un dia par");
     }else{
