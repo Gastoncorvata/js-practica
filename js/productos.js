719e987240d65a75b1f42fa6ffb1df3f64ff36ed
Gaston Corvalan <corvata@gmail.com>
2021-01-16 11:09:08 -0300

    filtrarProductos

---
+++ js/productos.js
@@ -0,0 +1,99 @@
+const dataProductos = [
+	{
+		imagen: "../img/burguer1.png",
+		nombre: "Springfield Bacon",
+		tipo: "Calories-980 / Carbs-40 / Sodium-1050",
+		description:
+			"Our regular two-patty burger, layered with four pieces of crispy, sweet applewood-smoked bacon.",
+		precio: "570",
+		id: "1",
+	},
+	{
+		imagen: "../img/burger3.png",
+		nombre: "Wisconsin Butter",
+		tipo: "Calories-930 / Carbs-38 / Sodium-950",
+		description:
+			"Wide selection of beef, pork, chicken, seafood, or veggie patties. regular two-patty burger.",
+		precio: "560",
+		id: "2",
+	},
+	{
+		imagen: "../img/burger4.png",
+		nombre: "Chicago Onions",
+		tipo: "Calories-850 / Carbs-22 / Sodium-760",
+		description:
+			"Two slices of Kraft American Cheese melted between two freshly grilled patties on a soft, toasted bun..",
+		precio: "550",
+		id: "3",
+	},
+	{
+		imagen: "../img/burger5.png",
+		nombre: "American Burger",
+		tipo: "Calories-980 / Carbs-40 / Sodium-1050",
+		description:
+			"Our regular two-patty burger, layered with four pieces of crispy, sweet applewood-smoked bacon.",
+		precio: "540",
+		id: "4",
+	},
+	{
+		imagen: "../img/burguer1.png",
+		nombre: "Springfield Bacon",
+		tipo: "Calories-980 / Carbs-40 / Sodium-1050",
+		description:
+			"Our regular two-patty burger, layered with four pieces of crispy, sweet applewood-smoked bacon.",
+		precio: "570",
+		id: "5",
+	},
+	{
+		imagen: "../img/burger3.png",
+		nombre: "Wisconsin Butter",
+		tipo: "Calories-980 / Carbs-40 / Sodium-1050",
+		description:
+			"Our regular two-patty burger, layered with four pieces of crispy, sweet applewood-smoked bacon.",
+		precio: "500",
+		id: "6",
+	},
+	{
+		imagen: "../img/burger4.png",
+		nombre: "Springfield Bacon",
+		tipo: "Calories-980 / Carbs-40 / Sodium-1050",
+		description:
+			"Our regular two-patty burger, layered with four pieces of crispy, sweet applewood-smoked bacon.",
+		precio: "490",
+		id: "7",
+	},
+	{
+		imagen: "../img/burger5.png",
+		nombre: "Wisconsin Butter",
+		tipo: "Calories-980 / Carbs-40 / Sodium-1050",
+		description:
+			"Our regular two-patty burger, layered with four pieces of crispy, sweet applewood-smoked bacon.",
+		precio: "480",
+		id: "8",
+	},
+	{
+		imagen: "../img/coca.png",
+		nombre: "Coca-Cola",
+		tipo: "Zero / Ligth / Regular",
+		description: "Botella plastico de 1ooo cc. Linea Coca-Cola. Sprite, Fanta, Coca, Cepita.",
+		precio: "150",
+		id: "9",
+	},
+	{
+		imagen: "../img/beer.png",
+		nombre: "Cerveza",
+		tipo: "Corona / Estrella / Sol",
+		description: "Botella vidrio o lata de 500 cc. Todos las variantes de Corona, Estrella o Sol.",
+		precio: "150",
+		id: "10",
+	},
+	{
+		imagen: "../img/franui.png",
+		nombre: "Fra-nui",
+		tipo: "Chocolate Amargo / Chocolate con Leche",
+		description: "Frambuesas bañadas en chocolate de Bariloche.",
+		precio: "270",
+		id: "11",
+	},
+];
+
--- js/proyecto2.js
+++ js/proyecto2.js
@@ -19,9 +19,83 @@ document.addEventListener("DOMContentLoaded", () => {
     insertarCarritoHTML();
 });
 
+//jQuery
+
+$("#formulario").on("submit", filtrarProductos);
+
+function cargarListaProductos(productos) {
+	productos.forEach((producto, index) => {
+		const { nombre, imagen, precio, id, tipo, description } = producto;
+
+		const divCard = document.createElement("div");
+		divCard.classList.add("col-12", "col-md-3", "py-4");
+		divCard.innerHTML = `
+			<div class="card col-12 col-md-3 py-4" style="width: 16rem;">
+                <img src="${imagen}" class="card-img-top" alt="Birra image">
+                <div class="card-body">
+                <h4 class="card-title text-justify text-center blanco">${nombre}</h4>
+                <h6 class="card-subtitle mb-2 text-muted text-center">${tipo}</h6>
+                <p class="card-text text-justify blanco">
+                ${description}</p>
+                <p class="precio blanco"> $ <span>${precio}</span></p>
+                <a href="#" class="btn btn-outline-light agregar-carrito" data-id="${id}">Agregar  <i class="fas fa-shopping-basket"></i></a>
+                </div>
+            </div>
+		`;
+		if (index % 3 === 0) {
+			const row = document.createElement("div");
+			row.classList.add("row");
+
+			listaProductos.appendChild(row);
+			row.appendChild(divCard);
+		} else {
+			const row = document.querySelector("#lista-productos .row:last-child");
+			row.appendChild(divCard);
+		}
+	});
+}
+
+function filtrarProductos(e) {
+	e.preventDefault();
+
+	const busqueda = $("#buscador").val();
+
+	const resultado = dataProductos.filter(producto => producto.nombre.toLocaleLowerCase().includes(busqueda.toLocaleLowerCase()));
+
+	limpiarProductos();
+	cargarListaProductos(resultado);
+}
 
 
 //Funtions
+
+/*function renderProductos() {
+	dataProductos.forEach((productos)=> {
+		const {imagen, nombre, tipo, description, precio, cantidad, id} = productos;
+		const fila = document.createElement("div");
+		fila.innerHTML = `
+		
+		<div class="row justify-content-between">
+			<div class="card col-12 col-md-3 py-4" style="width: 16rem;">
+				<img src=${imagen} class="card-img-top" alt="Burger image">
+				<div class="card-body">
+					<h4 class="card-title text-justify text-center blanco">${nombre}</h4>
+					<h6 class="card-subtitle mb-2 text-muted text-center">Calories-980 / Carbs-40 / Sodium-1050</h6>
+					<p class="card-text text-justify blanco">
+					Our regular two-patty burger, layered with four pieces of crispy, sweet applewood-smoked bacon.</p>
+					<p class="precio blanco"> $ <span>${precio}</span></p>
+					<a href="#" class="btn btn-outline-light agregar-carrito" data-id="${id}">Agregar  <i class="fas fa-shopping-basket"></i></a>
+				</div>
+			<div>
+		</div>
+		`;
+
+		listaProductos.appendChild(fila)
+	})
+}
+renderProductos();
+*/
+
 function agregarProducto(e) {
 	e.preventDefault();
 	if (e.target.classList.contains("agregar-carrito")) {
@@ -114,9 +188,12 @@ function insertarCarritoHTML() {
 }
 
 function limpiarCarrito() {
-	// contenedorCarrito.innerHTML = '';
-
 	while (contenedorCarrito.firstChild) {
 		contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+	}
+}
+function limpiarProductos() {
+	while (listaProductos.firstChild) {
+		listaProductos.removeChild(listaProductos.firstChild);
 	}
 }
